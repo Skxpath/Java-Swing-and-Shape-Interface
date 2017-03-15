dcb331462013b5104e002e122c4521dfd7e45be9
Alex Zhang <cwzhang@sfu.ca>
2017-03-14 22:42:22 -0700

    Created base TextBox class

---
+++ src/TextBox.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Aria on 3/14/2017.
+ */
+public class TextBox extends Rectangle {
+    TextBox(int xLocation, int yLocation, int width, int height) {
+        super(xLocation, yLocation, width, height);
+    }
+    
+}
